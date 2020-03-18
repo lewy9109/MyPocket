412e71e1c6ca0990ea1c7221a88930cdf4f1a0cc
lewy9109 <lewy9109@gmail.com>
2020-03-18 19:20:52 +0100

    start project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Budget
+//
+//  Created by Krystian Lewandowski on 18/03/2020.
+//  Copyright © 2020 Krystian Lewandowski. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    return 0;
+}
